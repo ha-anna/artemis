d80f58a6cd893c3a1dc84888a325896cca71a5aa
Anna Ha <its.haanna@gmail.com>
2024-05-14 15:18:33 +0900

    Add: Improve Map component

--- client/src/pages/components/Map.tsx
+++ client/src/pages/components/Map.tsx
@@ -1,54 +1,69 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-import mapboxgl, { LngLat } from 'mapbox-gl'
-import { useMap } from '@/lib/hooks/useMap'
+import mapboxgl, { LngLat, Map as MapboxMap } from 'mapbox-gl'
 
 export default function Map() {
-  const [location, setLocation] = useState<LngLat | null>(null)
+  const [map, setMap] = useState<MapboxMap | null>(null)
   const [consent, setConsent] = useState(false)
-  const map = useMap({
-    container: 'map-container',
-    style: 'mapbox://styles/mapbox/streets-v11',
-    center: new LngLat(126.9784, 37.5665),
-    zoom: 12,
-    accessToken: process.env.NEXT_PUBLIC_MAPBOX_KEY ?? ''
-  })
 
   useEffect(() => {
-    if (!consent || !map) return
+    const savedValue = window.localStorage.getItem('artemis-consent')
+    setConsent(savedValue == 'true' ?? false)
+  }, [])
 
-    if ('geolocation' in navigator) {
-      navigator.geolocation.getCurrentPosition(position => {
-        console.log(position.coords.latitude, position.coords.longitude)
-        setLocation(
-          new LngLat(position.coords.longitude, position.coords.latitude)
+  useEffect(() => {
+    const mapInstance = new mapboxgl.Map({
+      container: 'map-container',
+      style: 'mapbox://styles/mapbox/streets-v11',
+      center: new LngLat(126.9784, 37.5665),
+      zoom: 12,
+      accessToken: process.env.NEXT_PUBLIC_MAPBOX_KEY ?? ''
+    })
+
+    mapInstance.on('load', () => {
+      setMap(mapInstance)
+
+      mapInstance.addControl(
+        new mapboxgl.GeolocateControl({
+          trackUserLocation: consent,
+          showUserHeading: consent ? true : false,
+          showUserLocation: consent ? true : false
+        })
+      )
+
+      if (consent) {
+        navigator.geolocation.getCurrentPosition(
+          position => {
+            const { longitude, latitude } = position.coords
+            mapInstance.flyTo({ center: new LngLat(longitude, latitude) })
+          },
+          error => {
+            switch (error.code) {
+              case error.PERMISSION_DENIED:
+                console.log('User denied the request for Geolocation.')
+                break
+              case error.POSITION_UNAVAILABLE:
+                console.log('Location information is unavailable.')
+                break
+              case error.TIMEOUT:
+                console.log('The request to get user location timed out.')
+                break
+              default:
+                console.log('An unknown error occurred.')
+                break
+            }
+          }
         )
-      })
-    } else {
-      console.log('user consent failed.')
+      }
+    })
+
+    return () => {
+      if (mapInstance) mapInstance.remove()
     }
-
-    map.addControl(
-      new mapboxgl.GeolocateControl({
-        // When active the map will receive updates to the device's location as it changes.
-        trackUserLocation: true,
-        // Draw an arrow next to the location dot to indicate which direction the device is heading.
-        showUserHeading: true
-      })
-    )
-    // eslint-disable-next-line
   }, [consent])
-
-  useEffect(() => {
-    if (!location || !map) return
-
-    map.setCenter(location)
-    // eslint-disable-next-line
-  }, [location])
 
   return (
     <div className='rounded-lg mb-[300px] h-[80vh] p-4 mx-10 translate-y-[150px]'>
-      {/* <button onClick={() => setConsent(true)}>Agreed</button> */}
       <div
         id='map-container'
         className='rounded-lg h-full relative z-10'
