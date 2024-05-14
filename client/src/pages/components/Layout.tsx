1a14ef19b96b752076a3f38ce16dfd72cd6e150b
Anna Ha <its.haanna@gmail.com>
2024-05-14 15:18:21 +0900

    Add: Display Cookies Popup

--- client/src/pages/components/Layout.tsx
+++ client/src/pages/components/Layout.tsx
@@ -2,8 +2,28 @@ import Nav from './Nav'
 import Footer from './Footer'
 import Toast from './Toast'
 import Head from 'next/head'
+import CookiesPopup from './CookiesPopup'
+import { useEffect } from 'react'
+import {
+  selectConsent,
+  updateConsent
+} from '@/lib/store/features/consent/consentSlice'
+import { useAppDispatch, useAppSelector } from '@/lib/store/store'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const consent = useAppSelector(selectConsent).consent
+  const dispatch = useAppDispatch()
+
+  useEffect(() => {
+    const savedValue = window.localStorage.getItem('artemis-consent')
+    dispatch(updateConsent({ consent: savedValue == 'true' }))
+  }, [])
+
+  const handleConsent = () => {
+    localStorage.setItem('artemis-consent', 'true')
+    dispatch(updateConsent({ consent: true }))
+  }
+
   return (
     <>
       <Head>
@@ -14,6 +34,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <main className='flex-grow'>{children}</main>
         <Toast />
         <Footer />
+        {!consent && <CookiesPopup onConsent={handleConsent} />}
       </div>
     </>
   )
